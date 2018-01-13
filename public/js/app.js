aabf7b7095e437127c65de743fae525b3420df0e
liziscurious <32523128+liziscurious@users.noreply.github.com>
2018-01-13 13:45:48 -0800

    ng-repeat working for movie images

--- public/js/app.js
+++ public/js/app.js
@@ -4,29 +4,38 @@ const app = angular.module('dindin', []);
 app.controller('MainController', ['$http', function($http){
 
   // this.message='Liz is throwing away her salad';
+  this.movies = [];
 
-$http({
-  method: 'GET',
-  url: 'http://localhost:3000/movies'
-}).then(response=> {
-  console.log('response: ', response);
-  this.movies = response.data;
-}).catch(reject => {
-  console.log('reject: ', reject);
-});
-this.processForm = () => {
-  console.log('Form Data: ', this.formdata);
-  $http({
-    method:'POST',
-    url: 'http://localhost:3000/movies',
-    data: this.formdata
-  }).then(response => {
-    console.log('response: ', response);
-    this.movies.unshift(response.data);
-  }).catch(reject => {
-    console.log('reject: ', reject);
-  });
-};
+  // Show Movies Function
+  this.getAllMovies = () => {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:3000/movies'
+    }).then(response=> {
+      console.log('response: ', response);
+      this.movies = response.data;
+    }).catch(reject => {
+      console.log('reject: ', reject);
+    });
+  };
+
+  // Initiall show all movies call
+  this.getAllMovies();
+
+  // Create Movie
+  this.processForm = () => {
+    console.log('Form Data: ', this.formdata);
+    $http({
+      method:'POST',
+      url: 'http://localhost:3000/movies',
+      data: this.formdata
+    }).then(response => {
+      console.log('response: ', response);
+      this.movies.unshift(response.data);
+    }).catch(reject => {
+      console.log('reject: ', reject);
+    });
+  };
 
 
 
--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
-app.listen(PORT, () => console.log('APP ON PORT ' + PORT));
+app.listen(PORT, () => {
+  console.log("===================================================");
+  console.log("Dinner and a Movie App is running on port: ", PORT);
+  console.log("===================================================");
+})
