4b9f26cdc23f2646b49255597856c251ba20d853
Illmaticno1 <demetrius.robinson@icloud.com>
2018-01-13 18:09:59 -0500

    added some styling and a search box

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ app.controller('MainController', ['$http', function($http){
 
   // this.message='Liz is throwing away her salad';
   this.movies = [];
+  this.imgs = [];
+  // this.showmovie = {};
 
   // Show Movies Function
   this.getAllMovies = () => {
@@ -13,11 +15,35 @@ app.controller('MainController', ['$http', function($http){
       url: 'http://localhost:3000/movies'
     }).then(response=> {
       console.log('response: ', response);
+      console.log("===============================");
+      console.log(response.data[0].img_url);
+      console.log("===============================");
       this.movies = response.data;
     }).catch(reject => {
       console.log('reject: ', reject);
     });
+    // this.getOneMovie = ( movie ) => {
+    //   this.movies = movie;
+    //   console.log('++++++++++++++++++++++++');
+    //   console.log(this.movies);
+    //   console.log('++++++++++++++++++++++++');
+    // };
   };
+  // this.getOneMovie = () => {
+  //   $http({
+  //     method: 'GET',
+  //     url: 'http://localhost:3000/movies/:id'
+  //   }).then(response=> {
+  //     console.log('response: ', response);
+  //     console.log("===============================");
+  //     console.log(response.data[0].img_url);
+  //     console.log("===============================");
+  //     this.movies = response.data.id;
+  //   }).catch(reject => {
+  //     console.log('reject: ', reject);
+  //   });
+  // };
+  // this.getOneMovie();
 
   // Initiall show all movies call
   this.getAllMovies();
