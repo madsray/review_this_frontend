cc57159dad5d09631ecfa622ceeb644dfc307de7
madsray <30870145+madsray@users.noreply.github.com>
2018-01-13 15:13:12 -0600

    get and post routes added

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,34 @@
-$(document).ready(function() {
-    $('#myCarousel').carousel({
-	    interval: 10000
-	})
+// console.log("Winston is the best dog and maybe Dexter.")
+
+const app = angular.module('dindin', []);
+app.controller('MainController', ['$http', function($http){
+
+  // this.message='Liz is throwing away her salad';
+
+$http({
+  method: 'GET',
+  url: 'http://localhost:3000/movies'
+}).then(response=> {
+  console.log('response: ', response);
+  this.movies = response.data;
+}).catch(reject => {
+  console.log('reject: ', reject);
 });
+this.processForm = () => {
+  console.log('Form Data: ', this.formdata);
+  $http({
+    method:'POST',
+    url: 'http://localhost:3000/movies',
+    data: this.formdata
+  }).then(response => {
+    console.log('response: ', response);
+    this.movies.unshift(response.data);
+  }).catch(reject => {
+    console.log('reject: ', reject);
+  });
+};
+
+
+
+
+}])
