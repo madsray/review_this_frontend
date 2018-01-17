c78c6ff83d71856d110146b901def386d60734fe
madsray <30870145+madsray@users.noreply.github.com>
2018-01-17 10:25:44 -0600

    update route in progress

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,8 @@ app.controller('MainController', ['$http', function($http){
   this.formdata = {};
   this.reviews = [];
   this.movieID = 0;
+  this.displayEdit = false;
+  this.updateform = {};
 
 
   // Show Movies Function
@@ -100,4 +102,29 @@ this.deleteReview = (id) => {
     this.showOne(this.oneMovie);
   }).catch(err=> console.error(err.message));
 };
+
+this.editReview = (id) => {
+  console.log("Clicckkkk");
+  console.log(this.clickedId);
+
+  $http({
+    url: 'http://localhost:3000/reviews/' + id.id,
+    method: 'PUT',
+    data: {
+      title: this.updateform.title,
+      content: this.updateform.content,
+      rating: this.updateform.rating,
+      movie_id: this.updateform.id
+    }
+  }).then (response => {
+
+    console.log(response.data.id);
+    this.showOne(this.oneMovie);
+
+  },error=>console.error(error)).catch(err=> console.log(err.message));
+};
+
+
+
+
 }]);
